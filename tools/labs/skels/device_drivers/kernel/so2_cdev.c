1c181fabdb8c899151f3b48068cb1ebafda1a2a8
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-03-19 19:09:11 +0200

    ex 3
    Signed-off-by: Ruxandra Grigorie <ruxi.grigorie@gmail.com>

--- tools/labs/skels/device_drivers/kernel/so2_cdev.c
+++ tools/labs/skels/device_drivers/kernel/so2_cdev.c
@@ -35,7 +35,7 @@ MODULE_LICENSE("GPL");
 
 struct so2_device_data {
 	/* TODO 2: add cdev member */
-	
+	struct cdev cdev;	
 	/* TODO 4: add buffer with BUFSIZ elements */
 	/* TODO 7: extra members for home */
 	/* TODO 3: add atomic_t access variable to keep track if file is opened */
@@ -48,13 +48,17 @@ static int so2_cdev_open(struct inode *inode, struct file *file)
 	struct so2_device_data *data;
 
 	/* TODO 2: print message when the device file is open. */
+	pr_info("Cdev open\n");
+	
+
 
 	/* TODO 3: inode->i_cdev contains our cdev struct, use container_of to obtain a pointer to so2_device_data */
 
+	//data = container_of(inode->i_cdev, struct so2_device_data, cdev);
 	file->private_data = data;
 
 	/* TODO 3: return immediately if access is != 0, use atomic_cmpxchg */
-
+	
 	set_current_state(TASK_INTERRUPTIBLE);
 	schedule_timeout(10 * HZ);
 
@@ -65,7 +69,7 @@ static int
 so2_cdev_release(struct inode *inode, struct file *file)
 {
 	/* TODO 2: print message when the device file is closed. */
-
+	pr_info("Cdev closed\n");
 #ifndef EXTRA
 	struct so2_device_data *data =
 		(struct so2_device_data *) file->private_data;
@@ -129,6 +133,8 @@ so2_cdev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 static const struct file_operations so2_fops = {
 	.owner = THIS_MODULE,
 /* TODO 2: add open and release functions */
+	.open = so2_cdev_open,
+	.release = so2_cdev_release,
 /* TODO 4: add read function */
 /* TODO 5: add write function */
 /* TODO 6: add ioctl function */
@@ -161,6 +167,9 @@ static int so2_cdev_init(void)
 		/* TODO 7: extra tasks for home */
 		/* TODO 3: set access variable to 0, use atomic_set */
 		/* TODO 2: init and add cdev to kernel core */
+
+		cdev_init(&(devs[i].cdev), &so2_fops);
+		cdev_add(&(devs[i].cdev), MKDEV(MY_MAJOR, i), 1);
 	}
 
 	return 0;
@@ -172,6 +181,7 @@ static void so2_cdev_exit(void)
 
 	for (i = 0; i < NUM_MINORS; i++) {
 		/* TODO 2: delete cdev from kernel core */
+		cdev_del(&devs[i].cdev);
 	}
 
 	/* TODO 1: unregister char device region, for MY_MAJOR and NUM_MINORS starting at MY_MINOR */
