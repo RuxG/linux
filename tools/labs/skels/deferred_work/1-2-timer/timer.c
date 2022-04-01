5b8a77dad31706ced35bd16feef7181052eb0001
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-04-01 08:52:40 +0300

    1,2

--- tools/labs/skels/deferred_work/1-2-timer/timer.c
+++ tools/labs/skels/deferred_work/1-2-timer/timer.c
@@ -20,8 +20,9 @@ static struct timer_list timer;
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: print a message */
-
+	pr_info("Timer tick\n");
 	/* TODO 2: rechedule timer */
+	mod_timer(&timer, jiffies + HZ * TIMER_TIMEOUT);
 }
 
 static int __init timer_init(void)
@@ -29,9 +30,10 @@ static int __init timer_init(void)
 	pr_info("[timer_init] Init module\n");
 
 	/* TODO 1: initialize timer */
-
+	unsigned int flags;
+	timer_setup(&timer, timer_handler, flags); 
 	/* TODO 1: schedule timer for the first time */
-
+	mod_timer(&timer, jiffies + HZ * TIMER_TIMEOUT);
 	return 0;
 }
 
@@ -40,6 +42,7 @@ static void __exit timer_exit(void)
 	pr_info("[timer_exit] Exit module\n");
 
 	/* TODO 1: cleanup; make sure the timer is not running after we exit */
+	del_timer_sync(&timer);
 }
 
 module_init(timer_init);
