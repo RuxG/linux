e33220910d6e6ac1936d8d2e29287591d11e6c41
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-04-01 09:19:48 +0300

    3

--- tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
+++ tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
@@ -38,7 +38,9 @@ struct mon_proc {
 static struct my_device_data {
 	struct cdev cdev;
 	/* TODO 1: add timer */
+	struct timer_list timer;
 	/* TODO 2: add flag */
+
 	/* TODO 3: add work */
 	/* TODO 4: add list for monitored processes */
 	/* TODO 4: add spinlock to protect list */
@@ -81,6 +83,7 @@ static struct mon_proc *get_proc(pid_t pid)
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: implement timer handler */
+	pr_info("Tick tack\n");
 	/* TODO 2: check flags: TIMER_TYPE_SET or TIMER_TYPE_ALLOC */
 		/* TODO 3: schedule work */
 		/* TODO 4: iterate the list and check the proccess state */
@@ -115,9 +118,11 @@ static long deferred_ioctl(struct file *file, unsigned int cmd, unsigned long ar
 		case MY_IOCTL_TIMER_SET:
 			/* TODO 2: set flag */
 			/* TODO 1: schedule timer */
+			mod_timer(&(my_data->timer), jiffies + arg * HZ);
 			break;
 		case MY_IOCTL_TIMER_CANCEL:
 			/* TODO 1: cancel timer */
+			mod_timer(&(my_data->timer), 0);
 			break;
 		case MY_IOCTL_TIMER_ALLOC:
 			/* TODO 2: set flag and schedule timer */
@@ -163,7 +168,8 @@ static int deferred_init(void)
 	cdev_add(&dev.cdev, MKDEV(MY_MAJOR, MY_MINOR), 1);
 
 	/* TODO 1: Initialize timer. */
-
+	unsigned int flags;
+	timer_setup(&(dev.timer), timer_handler, flags);  
 	return 0;
 }
 
@@ -177,6 +183,7 @@ static void deferred_exit(void)
 	unregister_chrdev_region(MKDEV(MY_MAJOR, MY_MINOR), 1);
 
 	/* TODO 1: Cleanup: make sure the timer is not running after exiting. */
+	del_timer_sync(&(dev.timer));
 	/* TODO 3: Cleanup: make sure the work handler is not scheduled. */
 
 	/* TODO 4: Cleanup the monitered process list */
