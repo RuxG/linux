b3624dcd183ba1d542bbfec541ae0c08efb995f3
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-05-06 11:22:29 +0300

    Ex 6: Add implementation

--- tools/labs/skels/filesystems/myfs/myfs.c
+++ tools/labs/skels/filesystems/myfs/myfs.c
@@ -46,15 +46,21 @@ static const struct inode_operations myfs_dir_inode_operations = {
 };
 
 static const struct file_operations myfs_file_operations = {
-	/* TODO 6: Fill file operations structure. */
+	.llseek         = generic_file_llseek,
+	.read_iter      = generic_file_read_iter,
+	.write_iter     = generic_file_write_iter,
+	.mmap           = generic_file_mmap,
 };
 
 static const struct inode_operations myfs_file_inode_operations = {
-	/* TODO 6: Fill file inode operations structure. */
+	.setattr	= simple_setattr,
+	.getattr	= simple_getattr,
 };
 
 static const struct address_space_operations myfs_aops = {
-	/* TODO 6: Fill address space operations structure. */
+	.readpage		= simple_readpage,
+	.write_begin	= simple_write_begin,
+	.write_end		= simple_write_end,
 };
 
 struct inode *myfs_get_inode(struct super_block *sb, const struct inode *dir,
@@ -65,47 +71,29 @@ struct inode *myfs_get_inode(struct super_block *sb, const struct inode *dir,
 	if (!inode)
 		return NULL;
 
-	/* TODO 3: fill inode structure
-	 *     - mode
-	 *     - uid
-	 *     - gid
-	 *     - atime,ctime,mtime
-	 *     - ino
-	 */
 	inode_init_owner(inode, dir, mode);
 	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
-	//inode->i_ino = 1;
 	inode->i_ino = get_next_ino();
 
-
-	/* TODO 5: Init i_ino using get_next_ino */
-
-	/* TODO 6: Initialize address space operations. */
+	inode->i_mapping->a_ops = &myfs_aops;
 
 	if (S_ISDIR(mode)) {
-		/* TODO 3: set inode operations for dir inodes. */
-		inode->i_op = &simple_dir_inode_operations;
+		
+		//inode->i_op = &simple_dir_inode_operations;
 		inode->i_fop = &simple_dir_operations;
 
-		/* TODO 5: use myfs_dir_inode_operations for inode
-		 * operations (i_op).
-		 
-		 */
 		inode->i_op = &myfs_dir_inode_operations;
-		/* TODO 3: directory inodes start off with i_nlink == 2 (for "." entry).
-		 * Directory link count should be incremented (use inc_nlink).
-		 */
+		
 		inc_nlink(inode);
+	} else if (S_ISREG(mode)) {
+		inode->i_fop = &myfs_file_operations;
+		inode->i_op = &myfs_file_inode_operations;
 	}
 
-	/* TODO 6: Set file inode and file operations for regular files
-	 * (use the S_ISREG macro).
-	 */
+	
 
 	return inode;
 }
-
-/* TODO 5: Implement myfs_mknod, myfs_create, myfs_mkdir. */
 
 static int
 myfs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode, dev_t dev)
